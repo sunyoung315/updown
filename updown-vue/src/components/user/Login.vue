1d5b29528bd9e1de40d2ac27a459bdd820b1b66d
sunyoung315 <rlatjsdud315@gmail.com>
2023-11-17 18:05:33 +0900

    feat: weight기능 vue와 연결 중..

--- updown-vue/src/components/user/Login.vue
+++ updown-vue/src/components/user/Login.vue
@@ -7,11 +7,10 @@
             <h3>로그인</h3>
             <fieldset>
                 <label for="id">아 이 디</label>
-                <input type="text" id="id" v-model="loginUser.id" placeholder="아이디를 입력해주세요.">
+                <input type="text" id="id" name="id" v-model="loginUser.id" placeholder="아이디를 입력해주세요.">
                 <label for="password">비 밀 번 호</label>
-                <input type="password" id="password" v-model="loginUser.password" placeholder="비밀번호를 입력해주세요.">
+                <input type="password" id="password" name="password" v-model="loginUser.password" placeholder="비밀번호를 입력해주세요.">
                 <button @click="login">로그인</button>
-                <!-- <RouterLink :to="{name: 'today'}"><button>로그인</button></RouterLink> -->
             </fieldset>
             <RouterLink :to="{name: 'signup'}">회원가입</RouterLink>
         </div>
--- updown-vue/src/components/weight/WeightModify.vue
+++ updown-vue/src/components/weight/WeightModify.vue
@@ -1,10 +1,22 @@
 <template>
     <div>
-        <h4>몸무게 수정 창</h4>
+        <h2>체중</h2>
+        <img @click="modify" @save="save" class="cursor" style="width: 2.5em;" src="../../asset/icon/save.png" alt="저장">
+        <h4>오늘의 체중은?</h4>
+        <input type="number" v-model="store.todayWeight.nowWeight" :placeholder=weight>kg
     </div>
 </template>
 
 <script setup>
+import { useWeightStore } from '@/stores/weight';
+
+const store = useWeightStore();
+
+const weight = store.todayWeight.nowWeight;
+
+const modify = function() {
+    store.modifyWeight();
+}
 
 </script>
 
